28a454fc3889882063fbf0b4bfa55006f84c4e70
joharimacapundag <johari.macapundag@lorma.edu>
2026-02-25 21:23:44 +0800

    added gitignore

--- obj/Debug/net10.0/Mazada.AssemblyInfo.cs
+++ obj/Debug/net10.0/Mazada.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mazada")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93aa0f8e6cc99372770ba8ccf424bdcde2d94d1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mazada")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mazada")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
